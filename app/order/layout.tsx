5c9b5dc11b6efbb3aebde424a7e6e9312a61edea
emcarlos <carlosemc12@gmail.com>
2025-03-07 09:30:09 -0300

    feat(order): add ToastNotification component to display notifications in order layout

--- app/order/layout.tsx
+++ app/order/layout.tsx
@@ -1,18 +1,24 @@
+import { Fragment } from "react";
 import OrderSidebar from "../componets/order/OrderSidebar";
 import OrderSummary from "../componets/order/OrderSummary";
+import ToastNotification from "../componets/order/ui/ToastNotification";
 
 export default function layout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="md:flex">
-      <OrderSidebar />
-      <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
-        {children}
-      </main>
-      <OrderSummary />
-    </div>
+    <Fragment>
+      <div className="md:flex">
+        <OrderSidebar />
+        <main className="md:flex-1 md:h-screen md:overflow-y-scroll p-5">
+          {children}
+        </main>
+        <OrderSummary />
+      </div>
+
+      <ToastNotification />
+    </Fragment>
   );
 }
