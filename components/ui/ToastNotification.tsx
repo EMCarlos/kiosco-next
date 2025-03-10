4326e65862956425c169e87927c355cab0e9ea5b
emcarlos <carlosemc12@gmail.com>
2025-03-09 22:32:46 -0300

    fix: import in ToastNotification

--- components/ui/ToastNotification.tsx
+++ components/ui/ToastNotification.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.min.css";
 
 const ToastNotification = () => {
   return <ToastContainer />;
